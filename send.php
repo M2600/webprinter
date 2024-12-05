031c3a510f240470d8af2abf8a2ed84ea7d23ac0
M260 <41881281+M2600@users.noreply.github.com>
2024-12-05 18:50:22 +0900

    php version

---
+++ send.php
@@ -0,0 +1,15 @@
+<?php
+
+function exec_print($path){
+	$command = "/home/klab/print.sh ";
+	$command .= $path;
+	$ret = shell_exec($command);
+	echo $command;
+}
+
+$file = $_FILES['file']['tmp_name'];
+exec_print($file);
+//echo($file);
+
+
+
